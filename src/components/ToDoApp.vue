2aedb661e0b3befaef0f98e46b14fbd42dede296
mramgolam <mahima.ramgolam@prodigious.com>
2022-02-10 16:18:22 +0400

    block input on empty field

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -4,8 +4,8 @@
     <div class="form-container ">
       <form class="flex flex-col gap-2" @submit.prevent="addNewToDo">
         <label>Add new Todo</label>
-        <input v-model="newTodo" name="newTodo" class="border border-gray-200 rounded-md h-8 px-4"/>
-        <button class="bg-blue-300 px-4 py-2 rounded-md">Add New Todo</button>
+        <input v-model="newTodo"  name="newTodo" class="border border-gray-200 rounded-md h-8 px-4"/>
+        <button  type="button" id="addToDo" ref="addToDo" class="bg-blue-300 px-4 py-2 rounded-md">Add New Todo</button>
         <button @click="markAllDone()" class="bg-teal-300 px-4 py-2 rounded-md">Mark all done</button>
         <button @click="removeAll()" class="bg-red-300 px-4 py-2 rounded-md">Remove all toDos</button>
       </form>
@@ -27,13 +27,17 @@ export default {
     const newTodo = ref('');
     const todoList = ref([]);
 
+    //To do, disable button when input field is empty
+
     function addNewToDo() {
-        console.log('form was submitted');
+        if(newTodo.value !== '')
+        {
         todoList.value.push({
           id: Date.now(),
           done: false,
           content: newTodo.value
         })
+        }
 
         // clear text entered as input
         newTodo.value = "";
